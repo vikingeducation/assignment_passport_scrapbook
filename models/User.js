5e014a18dff20712be551ef56c3d2f6e8f232739
William Whitworth <william10charles@gmail.com>
2017-04-25 17:28:00 -0600

    fixed the twit api

--- models/User.js
+++ models/User.js
@@ -2,104 +2,108 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 const Twit = require("twit-promise");
 
-
-
 const UserSchema = mongoose.Schema({
-    displayName: {
-        type: String,
-        required: true
-    },
-    facebookId: {
-        type: String,
-        unique: true
-    },
-    twitterObj: {},
-    email: {
-        type: String,
-        unique: true
-    }
+  displayName: {
+    type: String,
+    required: true
+  },
+  facebookId: {
+    type: String,
+    unique: true
+  },
+  twitterObj: {},
+  email: {
+    type: String,
+    unique: true
+  }
 });
 
 UserSchema.plugin(uniqueValidator);
 
 UserSchema.statics.findOrCreateFacebook = function(profile) {
-    return User.findOne({
-        facebookId: profile.id
-    }).then(user => {
-        if (user) {
-            return user;
-        }
-        else {
-            return new User({
-                displayName: profile.displayName,
-                facebookId: profile.id,
-                email: profile.emails[0].value
-            }).save();
-        }
-    });
+  return User.findOne({
+    facebookId: profile.id
+  }).then(user => {
+    if (user) {
+      return user;
+    } else {
+      return new User({
+        displayName: profile.displayName,
+        facebookId: profile.id,
+        email: profile.emails[0].value
+      }).save();
+    }
+  });
 };
 
 UserSchema.statics.findOrCreateTwitter = function(
-    profile,
-    user,
-    token,
-    tokenSecret
+  profile,
+  user,
+  token,
+  tokenSecret
 ) {
-    return User.findOneAndUpdate({
-        _id: user._id
-    }, {
+  return User.findOneAndUpdate(
+    {
+      _id: user._id
+    },
+    {
+      twitterObj: {
+        twitterId: profile.id,
+        twitterToken: token,
+        twitterTokenSecret: tokenSecret
+      }
+    },
+    {
+      returnNewDocument: true
+    }
+  ).then(user => {
+    if (user) {
+      return user;
+    } else {
+      return new User({
+        displayName: profile.displayName,
         twitterObj: {
-            twitterId: profile.id,
-            twitterToken: token,
-            twitterTokenSecret: tokenSecret
+          twitterId: profile.id,
+          twitterToken: token,
+          twitterTokenSecret: tokenSecret
         }
-    }, {
-        returnNewDocument: true
-    }).then(user => {
-        if (user) {
-            return user;
-        }
-        else {
-            return new User({
-                displayName: profile.displayName,
-                twitterObj: {
-                    twitterId: profile.id,
-                    twitterToken: token,
-                    twitterTokenSecret: tokenSecret
-                }
-            }).save();
-        }
-    });
+      }).save();
+    }
+  });
 };
 
 UserSchema.methods.getAccounts = async function() {
-    console.log("this.twitObject is", this.twitterObj);
-    let accounts = [];
-    accounts.push({
-        name: "Twitter",
-        authorized: this.twitterObj
-    });
-    accounts.push({
-        name: "Facebook",
-        authorized: this.facebookId
-    });
-    return accounts;
+  console.log("this.twitObject is", this.twitterObj);
+  let accounts = [];
+  accounts.push({
+    name: "Twitter",
+    authorized: this.twitterObj
+  });
+  accounts.push({
+    name: "Facebook",
+    authorized: this.facebookId
+  });
+  return accounts;
 };
 
 UserSchema.methods.getTweets = async function() {
-    let twitterObj = this.twitterObj;
-    if (twitterObj) {
-        let T = new Twit({
-            consumer_key: process.env.TWITTER_CONSUMER_KEY,
-            consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-            access_token: twitterObj.twitterToken,
-            access_token_secret: twitterObj.twitterTokenSecret
-        });
-        return T.get("statuses/sample").then(tweets => {
-            return tweets;
-        });
-    }
-    return null;
+  let twitterObj = this.twitterObj;
+  if (twitterObj) {
+    let T = new Twit({
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token: twitterObj.twitterToken,
+      access_token_secret: twitterObj.twitterTokenSecret
+    });
+    return T.get("search/tweets", {
+      q: "this since:2011-07-11",
+      count: 100
+    }).then(tweets => {
+      console.log(tweets.data);
+      return tweets;
+    });
+  }
+  return null;
 };
 
 const User = mongoose.model("User", UserSchema);
