7812f194dff56769ec487df04852b4295cafdefc
Will Timpson <will.timpson@gmail.com>
2017-08-15 18:31:04 -0400

    Working github fetch!

--- models/User.js
+++ models/User.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const UserSchema = mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String },
   facebookId: { type: String, unique: true },
   facebookPhoto: String,
   githubId: { type: String, unique: true },
-  githubPhoto: String
+  githubPhoto: String,
+  githubData: [{ type: String }]
 });
 
 UserSchema.plugin(uniqueValidator);
--- strategies/github.js
+++ strategies/github.js
@@ -2,7 +2,7 @@ const GithubStrategy = require("passport-github").Strategy;
 const { User } = require("../models");
 const Github = require("github");
 
-const github = new Github({Promise: require("bluebird")})
+const github = new Github({ Promise: require("bluebird") });
 
 const githubStrategy = new GithubStrategy(
   {
@@ -14,25 +14,33 @@ const githubStrategy = new GithubStrategy(
   },
   async function(req, accessToken, refreshToken, profile, done) {
     try {
-      github.authenticate({type: "oauth", token: accessToken});
-      const repos = await github.repos.getForUser({username: profile.username, per_page: 5, sort: "updated"})
-      console.log(repos);
+      github.authenticate({ type: "oauth", token: accessToken });
+      const githubOpts = {
+        username: profile.username,
+        per_page: 5,
+        sort: "updated"
+      };
+      let githubData = await github.repos.getForUser(githubOpts);
+      githubData = githubData.data.map(repo => repo.name);
       const githubId = profile.id;
       const githubPhoto = profile.photos[0].value;
-      const name = profile.name;
+      const name = profile.name || profile.displayName;
 
       req.session.locals = req.session.locals || {};
       req.session.locals.name = name;
       req.session.locals.githubPhoto = githubPhoto;
+      req.session.locals.githubData = githubData;
 
       if (req.user) {
         req.user.githubId = githubId;
         req.user.githubPhoto = githubPhoto;
+        req.user.githubData = githubData;
         const user = await req.user.save();
         done(null, user);
       } else {
         let user = await User.findOne({ githubId });
-        user = user || (await User.create({ githubId, githubPhoto, name }));
+        const userOpts = { githubId, githubPhoto, name, githubData };
+        user = user || (await User.create(userOpts));
         done(null, user);
       }
     } catch (error) {
