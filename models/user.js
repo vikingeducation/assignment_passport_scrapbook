9b0a296e7d81b547b11fceec80bc5e0543b2745b
Lakshmi <lakshmi.nishthala@gmail.com>
2017-12-12 13:12:46 -0800

    Setting up new views

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const UserSchema = mongoose.Schema({
+  emailId: { type: String, required: true },
   displayName: { type: String, required: true },
   facebookId: { type: String, require: true, unique: true }
 });
