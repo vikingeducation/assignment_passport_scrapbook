af4935c5067f71060699fe9a5d5b4cccd636d971
EdwinYung <eyung7@gmail.com>
2017-12-12 11:32:48 -0500

    facebook integrated

---
+++ app.js
@@ -0,0 +1,218 @@
+const express = require('express');
+const app = express();
+
+// ----------------------------------------
+// App Variables
+// ----------------------------------------
+app.locals.appName = 'Passport';
+
+// ----------------------------------------
+// ENV
+// ----------------------------------------
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+// ----------------------------------------
+// Body Parser
+// ----------------------------------------
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// ----------------------------------------
+// Sessions/Cookies
+// ----------------------------------------
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+
+app.use(cookieParser());
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: [process.env.SESSION_SECRET || 'secret']
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
+// ----------------------------------------
+// Flash Messages
+// ----------------------------------------
+const flash = require('express-flash-messages');
+app.use(flash());
+
+// ----------------------------------------
+// Method Override
+// ----------------------------------------
+const methodOverride = require('method-override');
+const getPostSupport = require('express-method-override-get-post-support');
+
+app.use(
+  methodOverride(
+    getPostSupport.callback,
+    getPostSupport.options // { methods: ['POST', 'GET'] }
+  )
+);
+
+// ----------------------------------------
+// Referrer
+// ----------------------------------------
+app.use((req, res, next) => {
+  req.session.backUrl = req.header('Referer') || '/';
+  next();
+});
+
+// ----------------------------------------
+// Public
+// ----------------------------------------
+app.use(express.static(`${__dirname}/public`));
+
+// ----------------------------------------
+// Logging
+// ----------------------------------------
+const morgan = require('morgan');
+const morganToolkit = require('morgan-toolkit')(morgan, {
+  req: ['cookies' /*, 'signedCookies' */]
+});
+
+app.use(morganToolkit());
+
+// require Passport and the Local Strategy
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ----------------------------------------
+// Routes
+// ----------------------------------------
+// 1
+const User = require('./models/User');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+// 2
+const LocalStrategy = require('passport-local').Strategy;
+
+// 3
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    User.findOne({ username }, function(err, user) {
+      console.log(user);
+      if (err) return done(err);
+      if (!user || !user.validPassword(password)) {
+        return done(null, false, { message: 'Invalid username/password' });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+//4
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// facebook
+const FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    },
+    function(accessToken, refreshToken, profile, done) {
+      const facebookId = profile.id;
+      const displayName = profile.displayName;
+
+      console.log(profile);
+      User.findOne({ facebookId }, function(err, user) {
+        if (err) return done(err);
+
+        if (!user) {
+          // Create a new account if one doesn't exist
+          user = new User({ facebookId, displayName });
+          user.save((err, user) => {
+            if (err) return done(err);
+            done(null, user);
+          });
+        } else {
+          // Otherwise, return the extant user.
+          done(null, user);
+        }
+      });
+    }
+  )
+);
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get(
+  '/auth/facebook/callback',
+  passport.authenticate('facebook', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  })
+);
+
+//Home router
+const home = require('./routers/home');
+app.use('/', home);
+
+// ----------------------------------------
+// Template Engine
+// ----------------------------------------
+const expressHandlebars = require('express-handlebars');
+const helpers = require('./helpers');
+
+const hbs = expressHandlebars.create({
+  helpers: helpers,
+  partialsDir: 'views/',
+  defaultLayout: 'application'
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// ----------------------------------------
+// Server
+// ----------------------------------------
+const port = process.env.PORT || process.argv[2] || 3000;
+const host = 'localhost';
+
+let args;
+process.env.NODE_ENV === 'production' ? (args = [port]) : (args = [port, host]);
+
+args.push(() => {
+  console.log(`Listening: http://${host}:${port}\n`);
+});
+
+if (require.main === module) {
+  app.listen.apply(app, args);
+}
+
+// ----------------------------------------
+// Error Handling
+// ----------------------------------------
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  if (err.stack) {
+    err = err.stack;
+  }
+  res.status(500).render('errors/500', { error: err });
+});
+
+module.exports = app;
---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const UserSchema = mongoose.Schema({
+  username: { type: String, required: false, unique: true },
+  passwordHash: { type: String, required: false, unique: true },
+  displayName: { type: String, required: false },
+  facebookId: { type: String, require: false, unique: true }
+});
+
+UserSchema.plugin(uniqueValidator);
+
+UserSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.passwordHash);
+};
+
+UserSchema.virtual('password')
+  .get(function() {
+    return this._password;
+  })
+  .set(function(value) {
+    this._password = value;
+    this.passwordHash = bcrypt.hashSync(value, 8);
+  });
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
