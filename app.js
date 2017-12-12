c9e6a6e565e363dce390c8c1f508177dfb5bc530
Hai Nguyen <memyselfandhai@gmail.com>
2017-12-12 10:10:11 -0600

    facebook settings added

--- app.js
+++ app.js
@@ -1,139 +1,124 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
 
 // ----------------------------------------
 // App Variables
 // ----------------------------------------
-app.locals.appName = 'My App';
-
+app.locals.appName = "My App";
 
 // ----------------------------------------
 // ENV
 // ----------------------------------------
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
-
 
 // ----------------------------------------
 // Body Parser
 // ----------------------------------------
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 // ----------------------------------------
 // Sessions/Cookies
 // ----------------------------------------
-const cookieParser = require('cookie-parser');
-const cookieSession = require('cookie-session');
+const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
 
 app.use(cookieParser());
-app.use(cookieSession({
-  name: 'session',
-  keys: [
-    process.env.SESSION_SECRET || 'secret'
-  ]
-}));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [process.env.SESSION_SECRET || "secret"]
+  })
+);
 
 app.use((req, res, next) => {
   res.locals.session = req.session;
   next();
 });
 
-
 // ----------------------------------------
 // Flash Messages
 // ----------------------------------------
-const flash = require('express-flash-messages');
+const flash = require("express-flash-messages");
 app.use(flash());
-
 
 // ----------------------------------------
 // Method Override
 // ----------------------------------------
-const methodOverride = require('method-override');
-const getPostSupport = require('express-method-override-get-post-support');
+const methodOverride = require("method-override");
+const getPostSupport = require("express-method-override-get-post-support");
 
-app.use(methodOverride(
-  getPostSupport.callback,
-  getPostSupport.options // { methods: ['POST', 'GET'] }
-));
-
+app.use(
+  methodOverride(
+    getPostSupport.callback,
+    getPostSupport.options // { methods: ['POST', 'GET'] }
+  )
+);
 
 // ----------------------------------------
 // Referrer
 // ----------------------------------------
 app.use((req, res, next) => {
-  req.session.backUrl = req.header('Referer') || '/';
+  req.session.backUrl = req.header("Referer") || "/";
   next();
 });
-
 
 // ----------------------------------------
 // Public
 // ----------------------------------------
 app.use(express.static(`${__dirname}/public`));
 
-
 // ----------------------------------------
 // Logging
 // ----------------------------------------
-const morgan = require('morgan');
-const morganToolkit = require('morgan-toolkit')(morgan, {
-  req: ['cookies'/*, 'signedCookies' */]
+const morgan = require("morgan");
+const morganToolkit = require("morgan-toolkit")(morgan, {
+  req: ["cookies" /*, 'signedCookies' */]
 });
 
 app.use(morganToolkit());
 
-
 // ----------------------------------------
 // Routes
 // ----------------------------------------
-app.use('/', (req, res) => {
-  req.flash('Hi!');
-  res.render('welcome/index');
+app.use("/", (req, res) => {
+  req.flash("Hi!");
+  res.render("welcome/index");
 });
-
 
 // ----------------------------------------
 // Template Engine
 // ----------------------------------------
-const expressHandlebars = require('express-handlebars');
-const helpers = require('./helpers');
+const expressHandlebars = require("express-handlebars");
+const helpers = require("./helpers");
 
 const hbs = expressHandlebars.create({
   helpers: helpers,
-  partialsDir: 'views/',
-  defaultLayout: 'application'
+  partialsDir: "views/",
+  defaultLayout: "application"
 });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // ----------------------------------------
 // Server
 // ----------------------------------------
-const port = process.env.PORT ||
-  process.argv[2] ||
-  3000;
-const host = 'localhost';
+const port = process.env.PORT || process.argv[2] || 3000;
+const host = "localhost";
 
 let args;
-process.env.NODE_ENV === 'production' ?
-  args = [port] :
-  args = [port, host];
+process.env.NODE_ENV === "production" ? (args = [port]) : (args = [port, host]);
 
 args.push(() => {
-  console.log(`Listening: http://${ host }:${ port }\n`);
+  console.log(`Listening: http://${host}:${port}\n`);
 });
 
 if (require.main === module) {
   app.listen.apply(app, args);
 }
-
 
 // ----------------------------------------
 // Error Handling
@@ -146,14 +131,54 @@ app.use((err, req, res, next) => {
   if (err.stack) {
     err = err.stack;
   }
-  res.status(500).render('errors/500', { error: err });
+  res.status(500).render("errors/500", { error: err });
 });
 
+// ----------------------------------------
+// Facebook Authentication
+// ----------------------------------------
+const FacebookStrategy = require("passport-facebook").Strategy;
+const passport = require("passport");
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: "http://localhost:4000/auth/facebook/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+      const facebookId = profile.id;
+      const displayName = profile.displayName;
+
+      console.log(profile);
+      User.findOne({ facebookId }, function(err, user) {
+        if (err) return done(err);
+
+        if (!user) {
+          // Create a new account if one doesn't exist
+          user = new User({ facebookId, displayName });
+          user.save((err, user) => {
+            if (err) return done(err);
+            done(null, user);
+          });
+        } else {
+          // Otherwise, return the extant user.
+          done(null, user);
+        }
+      });
+    }
+  )
+);
+
+app.get("/auth/facebook", passport.authenticate("facebook"));
+
+app.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+  })
+);
 
 module.exports = app;
-
-
-
-
-
-
