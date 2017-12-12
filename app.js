e77967a88eaed1351b929126574b7127cbf18bf9
Steven Li <steven4354@gmail.com>
2017-12-12 15:56:32 -0800

    google

--- app.js
+++ app.js
@@ -273,6 +273,47 @@ app.get(
 );
 
 // ----------------------------------------
+// Google Strategy
+// ----------------------------------------
+
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/auth/google/callback'
+    },
+    async function(accessToken, refreshToken, profile, done) {
+      try {
+        console.log(profile);
+        // const twitterId = profile.id;
+        // const displayName = profile.displayName;
+        // const followers = profile._json.followers_count;
+        //
+        // let user = new User({ twitterId, followers, displayName });
+        // await user.save();
+
+        done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+app.get('/auth/google', passport.authenticate('google'));
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('twitter', {
+    successRedirect: '/register',
+    failureRedirect: '/login'
+  })
+);
+
+// ----------------------------------------
 // Redirect to Routers
 // ----------------------------------------
 const home = require('./routers/home');
